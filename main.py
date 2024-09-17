f80c23749f2f9fba60c81f34346d0341420851a7
Nathan Foo <ntfoo27@outlook.com>
2024-09-17 13:40:43 -0500

    Import joblib

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-# import joblib
+import joblib
 
 music_data = pd.read_csv('music.csv')
 X = music_data.drop(columns=['genre'])
 y = music_data['genre']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = DecisionTreeClassifier()
 model.fit(X.values, y)
-model.fit(X_train, y_train)
+# model.fit(X_train, y_train)
 
-# model = joblib.load('music-recommender.joblib')
+model = joblib.dump(model, 'music-recommender.joblib')
 # predictions = model.predict([[21, 1]])
 
-predictions = model.predict(X_test)
-score = accuracy_score(y_test, predictions)
+# predictions = model.predict(X_test)
+# score = accuracy_score(y_test, predictions)
 
-print(predictions)
-print(score)
+# print(predictions)
+# print(score)
